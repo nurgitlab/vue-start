e1b5653b016e33e8f2f2330271c8870ac0e705ce
nurgitlab <nurbek.math@gmail.com>
2023-10-08 14:25:14 +0300

    learned: counters

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    Hello, Vue!
+    <div>Кол-во лайков <strong>{{ likes }}</strong></div>
+    <div>Кол-во дизлайков <strong>{{ dislikes }}</strong></div>
+    <button @click="addLike">+Like</button>
+    <button @click="addDislike">+Dislikes</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App.vue"
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes+=1
+    },
+    addDislike() {
+      this.dislikes+=1
+    }
+  }
 }
 </script>
 
